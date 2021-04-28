145a979f43c69f3c325466a5b405b384bceb2c87
yanpeicai <peicai.y@gmail.com>
2021-04-28 09:49:30 +0800

    automaticly open browser

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
-const { addMocksToSchema } = require('@graphql-tools/mock');
-const { loadSchemaSync } = require('@graphql-tools/load');
-const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
 
-const casual = require('casual');
+const casual = require("casual");
 
-const app = express()
+const app = express();
 
 // 读取指定路径下SDL文件
-const schema = loadSchemaSync('./schema/**/*.graphql', { // load from multiple files using glob
-    loaders: [
-        new GraphQLFileLoader()
-    ]
+const schema = loadSchemaSync("./schema/**/*.graphql", {
+  // load from multiple files using glob
+  loaders: [new GraphQLFileLoader()],
 });
 
 // 针对指定数据类型，mock返回特定的返回值
 const mocks = {
-    Int: () => casual.integer(from = 0, to = 10000),
-    Float: () => casual.double(from = 0, to = 10000),
-    String: () => casual.title,
-    Long: () => casual.integer(from = 0, to = 10000),
-    Date: () => casual.date(format = 'YYYY-MM-DDTHH:mm:ss.SSSZZ') // Output Example: 2011-11-11T11:43:31.000-0430
+  Int: () => casual.integer((from = 0), (to = 10000)),
+  Float: () => casual.double((from = 0), (to = 10000)),
+  String: () => casual.title,
+  Long: () => casual.integer((from = 0), (to = 10000)),
+  Date: () => casual.date((format = "YYYY-MM-DDTHH:mm:ss.SSSZZ")), // Output Example: 2011-11-11T11:43:31.000-0430
 };
 
 const preserveResolvers = false;
 
 const schemaWithMocks = addMocksToSchema({ schema, mocks, preserveResolvers });
 
-app.use('/graphql', graphqlHTTP({
-  schema: schemaWithMocks,
-  graphiql: true,
-}));
+app.use(
+  "/",
+  graphqlHTTP({
+    schema: schemaWithMocks,
+    graphiql: true,
+  })
+);
 
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+const port = 4000;
+app.listen(port);
+var url = "http://localhost:" + port;
+console.log("🚀 Running a GraphQL API server at " + url);
+// open url
+var start = process.platform == "darwin" ? "open" : process.platform == "win32" ? "start" : "xdg-open";
+require("child_process").exec(start + " " + url);
